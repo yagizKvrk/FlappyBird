2bb01c2542b8dd0ed0fc3293d769e5b97f401bcd
yağız kıvrak <yagizkivrak35.yk@gmail.com>
2022-10-20 16:13:16 +0300

    Add project files.

---
+++ FlappyBird/Form1.Designer.cs
@@ -0,0 +1,177 @@
+﻿namespace FlappyBird
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.btnBasla = new System.Windows.Forms.Button();
+            this.lblScoreWriting = new System.Windows.Forms.Label();
+            this.lblScore = new System.Windows.Forms.Label();
+            this.tmrIlerle = new System.Windows.Forms.Timer(this.components);
+            this.pbCim = new System.Windows.Forms.PictureBox();
+            this.pbUstEngel = new System.Windows.Forms.PictureBox();
+            this.pbAltEngel = new System.Windows.Forms.PictureBox();
+            this.pbBird = new System.Windows.Forms.PictureBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.pbCim)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbUstEngel)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbAltEngel)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbBird)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnBasla
+            // 
+            this.btnBasla.Location = new System.Drawing.Point(13, 527);
+            this.btnBasla.Margin = new System.Windows.Forms.Padding(4);
+            this.btnBasla.Name = "btnBasla";
+            this.btnBasla.Size = new System.Drawing.Size(155, 45);
+            this.btnBasla.TabIndex = 3;
+            this.btnBasla.Text = "Start";
+            this.btnBasla.UseVisualStyleBackColor = true;
+            this.btnBasla.Click += new System.EventHandler(this.btnBasla_Click);
+            // 
+            // lblScoreWriting
+            // 
+            this.lblScoreWriting.AutoSize = true;
+            this.lblScoreWriting.BackColor = System.Drawing.Color.Transparent;
+            this.lblScoreWriting.ForeColor = System.Drawing.SystemColors.Info;
+            this.lblScoreWriting.Location = new System.Drawing.Point(13, 9);
+            this.lblScoreWriting.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblScoreWriting.Name = "lblScoreWriting";
+            this.lblScoreWriting.Size = new System.Drawing.Size(64, 20);
+            this.lblScoreWriting.TabIndex = 0;
+            this.lblScoreWriting.Text = "Score:";
+            // 
+            // lblScore
+            // 
+            this.lblScore.AutoSize = true;
+            this.lblScore.BackColor = System.Drawing.Color.Transparent;
+            this.lblScore.ForeColor = System.Drawing.SystemColors.Info;
+            this.lblScore.Location = new System.Drawing.Point(84, 9);
+            this.lblScore.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblScore.Name = "lblScore";
+            this.lblScore.Size = new System.Drawing.Size(19, 20);
+            this.lblScore.TabIndex = 0;
+            this.lblScore.Text = "0";
+            // 
+            // tmrIlerle
+            // 
+            this.tmrIlerle.Interval = 20;
+            this.tmrIlerle.Tick += new System.EventHandler(this.tmrIlerle_Tick);
+            // 
+            // pbCim
+            // 
+            this.pbCim.Image = global::FlappyBird.Properties.Resources.flappyBirdFloor;
+            this.pbCim.Location = new System.Drawing.Point(1, 579);
+            this.pbCim.Name = "pbCim";
+            this.pbCim.Size = new System.Drawing.Size(1186, 75);
+            this.pbCim.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pbCim.TabIndex = 6;
+            this.pbCim.TabStop = false;
+            // 
+            // pbUstEngel
+            // 
+            this.pbUstEngel.Image = global::FlappyBird.Properties.Resources.boru2;
+            this.pbUstEngel.Location = new System.Drawing.Point(1049, -2);
+            this.pbUstEngel.Margin = new System.Windows.Forms.Padding(4);
+            this.pbUstEngel.Name = "pbUstEngel";
+            this.pbUstEngel.Size = new System.Drawing.Size(138, 135);
+            this.pbUstEngel.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pbUstEngel.TabIndex = 2;
+            this.pbUstEngel.TabStop = false;
+            // 
+            // pbAltEngel
+            // 
+            this.pbAltEngel.Image = global::FlappyBird.Properties.Resources.boru1;
+            this.pbAltEngel.Location = new System.Drawing.Point(668, 359);
+            this.pbAltEngel.Margin = new System.Windows.Forms.Padding(4);
+            this.pbAltEngel.Name = "pbAltEngel";
+            this.pbAltEngel.Size = new System.Drawing.Size(138, 295);
+            this.pbAltEngel.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pbAltEngel.TabIndex = 1;
+            this.pbAltEngel.TabStop = false;
+            // 
+            // pbBird
+            // 
+            this.pbBird.Image = global::FlappyBird.Properties.Resources.top_flying_bird_stickers_for_android_ios_gfycat;
+            this.pbBird.Location = new System.Drawing.Point(88, 209);
+            this.pbBird.Margin = new System.Windows.Forms.Padding(4);
+            this.pbBird.Name = "pbBird";
+            this.pbBird.Size = new System.Drawing.Size(80, 70);
+            this.pbBird.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pbBird.TabIndex = 0;
+            this.pbBird.TabStop = false;
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 70;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.SkyBlue;
+            this.ClientSize = new System.Drawing.Size(1187, 654);
+            this.Controls.Add(this.pbBird);
+            this.Controls.Add(this.pbCim);
+            this.Controls.Add(this.lblScore);
+            this.Controls.Add(this.lblScoreWriting);
+            this.Controls.Add(this.btnBasla);
+            this.Controls.Add(this.pbUstEngel);
+            this.Controls.Add(this.pbAltEngel);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyUp);
+            ((System.ComponentModel.ISupportInitialize)(this.pbCim)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbUstEngel)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbAltEngel)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbBird)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pbBird;
+        private System.Windows.Forms.PictureBox pbAltEngel;
+        private System.Windows.Forms.PictureBox pbUstEngel;
+        private System.Windows.Forms.Button btnBasla;
+        private System.Windows.Forms.Label lblScoreWriting;
+        private System.Windows.Forms.Label lblScore;
+        private System.Windows.Forms.Timer tmrIlerle;
+        private System.Windows.Forms.PictureBox pbCim;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ FlappyBird/Form1.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FlappyBird
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        int score = 0;
+        int gravity = 10;
+
+        Random uzaklikRnd = new Random();
+        Random hghtRnd = new Random();
+        private void btnBasla_Click(object sender, EventArgs e)
+        {
+            btnBasla.Hide();
+            tmrIlerle.Start();
+            btnBasla.Enabled = false;
+            pbBird.Enabled = true;
+        }
+
+        private void tmrIlerle_Tick(object sender, EventArgs e)
+        {
+            lblScore.Text = score.ToString();
+            pbBird.Top += gravity;
+
+            pbAltEngel.Left -= 9;
+            pbUstEngel.Left -= 9;
+            //Location bilgisi alınabilir ancak loc. değiştirilemiyor??
+            //if(pbAltEngel.Location.X+pbAltEngel.Width == 0)
+            //{
+            //    pbAltEngel.Left = 1300;
+            //}
+
+            if (pbAltEngel.Left < 0 - pbAltEngel.Width)
+            {
+                score++;
+                pbAltEngel.Left = uzaklikRnd.Next(1250,1500);
+                pbAltEngel.Top = hghtRnd.Next(pbAltEngel.Top, pbAltEngel.Top+170);
+            }
+
+            if (pbUstEngel.Left < 0 - pbUstEngel.Width)
+            {
+                score++;
+                pbUstEngel.Left = uzaklikRnd.Next(1250, 1500);
+                pbUstEngel.Height = hghtRnd.Next(125, 295);
+            }
+            if(pbBird.Bounds.IntersectsWith(pbUstEngel.Bounds) || pbBird.Bounds.IntersectsWith(pbAltEngel.Bounds) || pbBird.Bounds.IntersectsWith(pbCim.Bounds) || pbBird.Top <= 0)
+            {
+                tmrIlerle.Stop();
+                timer1.Stop();
+                DialogResult dr = MessageBox.Show("Game Over! Tekrar oynamak ister misiniz?", "Flappy Birdcüler", MessageBoxButtons.YesNo);
+                if(dr==DialogResult.Yes)
+                {
+                    lblScore.Text = "0";
+                    score = 0;
+                    pbBird.Left = 88;
+                    pbBird.Top = 209;
+                    pbAltEngel.Left = 668;
+                    pbAltEngel.Top = 359;
+                    pbUstEngel.Left = 1049;
+                    pbUstEngel.Top = -2;
+                    btnBasla.Show();
+                    btnBasla.Enabled = true;
+                }
+            }
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if(btnBasla.Enabled == true)
+            {
+                e.Handled = true;
+            }
+            else
+            {
+                if (e.KeyCode == Keys.Space)
+                {
+                    timer1.Start();
+                }
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            
+            pbBird.Top -= 100;
+        }
+
+        private void Form1_KeyUp(object sender, KeyEventArgs e)
+        {
+            timer1.Stop();
+        }
+        //En yüksek skor eklenebilir. Game over dan sonra skorunuzu sitemize eklemek ister misiniz diye sorup eğer istiyorsa başka bir form sayfasında listeye eklenebilir. Bu bilgileri dosyaya atıp program kapansa bile silinmemesini sağlarız. Eğer kişi kendi skorunu geçtiyse yeni yüksek skoru sisteme kaydedip eskiyi sileriz.
+    }
+}
